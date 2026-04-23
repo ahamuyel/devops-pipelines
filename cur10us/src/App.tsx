364ad73c82aec40edddf637e6e9d4291d4b97e00
ahamuyel <ahamuyel@cur10us.myguest.virtualbox.org>
2026-04-23 17:17:35 +0100

    docker compose

--- cur10us/src/App.tsx
+++ cur10us/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Cur10usX is the future</h1>
+          <h1>Get started</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
